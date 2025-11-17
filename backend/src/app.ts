acac4569a4a737aeca420ce946857d0ebabf0281
sanskardudhe09 <sanskardudhe2018@gmail.com>
2025-11-17 19:35:42 +0530

    fix frontend deploy url on backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,7 @@ export const createApp = (): Application => {
   app.use(helmet());
   app.use(
     cors({
-      origin: ['http://localhost:5173', 'https://enquiry-management-mern-frontend.vercel.app/'],
+      origin: ['http://localhost:5173', 'https://enquiry-management-mern-frontend.vercel.app'],
       credentials: true,
     })
   );
